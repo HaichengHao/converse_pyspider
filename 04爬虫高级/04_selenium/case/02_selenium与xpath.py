205cd377238f33ed4cbef38f5c00b7cf6fc00daa
hreo <2020104216@qq.com>
2024-10-14 23:34:28 +0800

    2024/10/6

---
+++ 04爬虫高级/04_selenium/case/02_selenium与xpath.py
@@ -0,0 +1,20 @@
+# @Editor    : 百年
+# @FileName  :02_selenium与xpath.py
+# @Time      :2024/10/14 22:53
+from selenium.webdriver import Chrome
+import time
+from lxml import etree
+
+# 构建浏览器
+browser1 = Chrome(executable_path='../others/chromedriver.exe')
+
+browser1.get('https://ypk.39.net/ganmao/k1')
+time.sleep(2)
+page_ = browser1.page_source
+tree = etree.HTML(page_)
+li_list = tree.xpath('/html/body/div[4]/div[3]/div[2]/div[2]/div[2]/div/ul/li')
+# print(li_list)
+time.sleep(1)
+for li in li_list:
+    title = li.xpath("./div[@class='drugs-brief']/p/a/@title")[0]
+    print(title)
---
+++ 04爬虫高级/04_selenium/code/08_基础补充.py
@@ -0,0 +1,35 @@
+# @Editor    : 百年
+# @FileName  :08_基础补充.py
+# @Time      :2024/10/7 20:55
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+
+# 构建浏览器
+browser = Chrome(executable_path='../others/chromedriver.exe')
+
+browser.get('https://www.jd.com/')
+
+time.sleep(3)
+# 定位到搜索栏
+serach_input = browser.find_element_by_xpath('/html/body/div[1]/div[4]/div/div[2]/div/div[2]/input')
+# 输入搜索内容
+serach_input.send_keys('轻薄本')
+time.sleep(2)
+
+# 点击搜索
+btn = browser.find_element_by_xpath('/html/body/div[1]/div[4]/div/div[2]/div/div[2]/button')
+btn.click()
+
+time.sleep(2)
+
+browser.switch_to.window(browser.window_handles[-1])
+time.sleep(2)
+
+
+# 执行js代码进行下滑
+browser.execute_script(
+    script="""
+    document.documentElement.scrollTo(0,1000)
+    """)
