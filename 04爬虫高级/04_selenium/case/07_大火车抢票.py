60d816df6d5cd92b64a9e886081a4b5eb8233077
aya <2020104216@qq.com>
2024-10-22 20:39:06 +0800

    2024/10/22

---
+++ 04爬虫高级/04_selenium/case/07_大火车抢票.py
@@ -0,0 +1,9 @@
+# @Author    : 百年
+# @FileName  :07_大火车抢票.py
+# @DateTime  :2024/10/22 19:19
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+
+# 创建浏览器对象
+browser1 = Chrome(executable_path='../others/chromedriver.exe')
