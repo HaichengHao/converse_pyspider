7364179f5474c4dbfa34da983fe4ff597857f594
niko <2020104216@qq.com>
2025-03-30 00:20:28 +0800

    20250329A

---
+++ 12app逆向/00md5加密.py
@@ -0,0 +1,10 @@
+"""
+@File    :00md5加密.py
+@Editor  : 百年
+@Date    :2025/3/29 18:56 
+"""
+import hashlib
+pwd = '12345'
+obj = hashlib.md5(pwd.encode('utf-8'))
+sign = obj.hexdigest()
+print(sign)
