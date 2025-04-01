b649f68151754421c75ef45e4d8982dfa563a367
niko <2020104216@qq.com>
2025-04-01 16:03:27 +0800

    20250401A

--- 11web逆向/08有道翻译_时间戳/yd.py
+++ 11web逆向/08有道翻译_时间戳/yd.py
@@ -5,33 +5,58 @@
 """
 import requests
 import fake_useragent
+
 useragent = fake_useragent.UserAgent.random
 import execjs
 import time
-url = 'https://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
-headers={
-    'User-Agent':useragent
 
+url = 'https://dict.youdao.com/webtranslate'
+headers = {
+    'User-Agent': useragent
 }
 word = input('输入单词>>')
-data = {
-    'i':word,
-    "from":"AUTO",
-    "smartresult":"dict",
-    "client":"fanyideskweb",
 
-}
+
 # 创建节点
 node = execjs.get()
 
 # 读取js文件创建上下文对象
-with open('youdao.js','r',encoding='utf-8') as fp:
-    ctx = node.compile(fp.read()) #important:调用节点对js文件进行预编译
-e='puppy' #传入要翻译的单词
+with open('youdao.js', 'r', encoding='utf-8') as fp:
+    ctx = node.compile(fp.read())  # important:调用节点对js文件进行预编译
+e = 'puppy'  # 传入要翻译的单词
 pytime = time.time()
-i = int(pytime*1000)
+i = int(pytime * 1000)
 print(i)
 funcname = f'getSign("{e}","{i}")'
 print(funcname)
 result = ctx.eval(funcname)
 print(result)
+data = {
+    'i': word,
+    "from": "AUTO",
+    "smartresult": "dict",
+    "client": "fanyideskweb",
+    "i": "dog",
+    "from": "auto",
+    'to': '',
+    "useTerm": "false",
+    "dictResult": "true",
+    "keyid": "webfanyi",
+    "sign": result,
+    "client": "fanyideskweb",
+    "product": "webfanyi",
+    "appVersion": "1 .0.0",
+    "vendor": "web",
+    "pointParam": "client,mysticTime,product",
+    "mysticTime": "1743482642347",
+    "keyfrom": "fanyi.web",
+    "mid": "1",
+    "screen": "1",
+    "model": "1",
+    "network": "wifi",
+    "abtest": "0",
+    "yduuid": "abcdefg",
+
+}
+
+response = requests.post(url=url,headers=headers,data=data)
---
+++ 杂项/超详细：python pycharm 正则表达式批量替换.md
@@ -0,0 +1,51 @@
+ 
+
+      在使用python爬虫中，需要对复制好的网页代码进行[正则表达式](https://so.csdn.net/so/search?q=%E6%AD%A3%E5%88%99%E8%A1%A8%E8%BE%BE%E5%BC%8F&spm=1001.2101.3001.7020)批量替换，这时候需要借助EditPlus等工具进行替换，但是在pycharm中也可以使用快捷命令进行替换。
+
+(csdn上写这个方便自己复制替换QAQ)
+
+**目录**
+
+[一、打开pycharm，将复制好的信息粘贴](#t0)
+
+[二、输入替换格式](#t1)
+
+[三、选中需要替换的部分](#t2)
+
+[四、点击全部替换](#t3)
+
+* * *
+
+### 一、打开pycharm，将复制好的信息粘贴
+
+使用快捷键Ctrl+R，打开替换界面
+
+![](https://i-blog.csdnimg.cn/blog_migrate/d9bcf58228fe59077b9bcc0a6398f31c.png)
+
+### 二、输入替换格式
+
+在第一个框输入
+
+```lisp
+(.*?):(.*)
+```
+
+第二个框输入
+
+```dart
+'$1':'$2',
+```
+
+结果如下：
+
+![](https://i-blog.csdnimg.cn/blog_migrate/22aff3e519b2c247667f21b63b4748f6.png)
+
+### 三、选中需要替换的部分
+
+![](https://i-blog.csdnimg.cn/blog_migrate/fe2aaa1c17d1157cd0052078f626b231.png)
+
+### 四、点击全部替换
+
+![](https://i-blog.csdnimg.cn/blog_migrate/52e747abe558b49f7ccb93e6f7a9d0c5.png)
+
+本文转自 <https://blog.csdn.net/MrPandada/article/details/133831536>，如有侵权，请联系删除。
