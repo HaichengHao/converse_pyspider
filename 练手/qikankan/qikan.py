8fef0cea1c01dfdc4dff4b1e962770663c726af4
aya <2020104216@qq.com>
2025-04-14 14:51:43 +0800

    20250414B

--- 练手/qikankan/qikan.py
+++ 练手/qikankan/qikan.py
@@ -4,10 +4,13 @@
 @Date    :2025/4/13 14:35 
 """
 from urllib.parse import urljoin
-
+import csv
 import requests
 from lxml import etree
-
+# fp = open('./full_url.csv','a+',encoding='utf-8',newline='')
+# writer = csv.writer(fp)
+# if fp.tell() == 0:
+#     writer.writerow(['所有类别的链接',])
 url = 'https://www.iikx.com/sci/'
 headers={
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'
@@ -17,9 +20,16 @@ response.encoding = response.apparent_encoding
 page_source = response.text
 tree = etree.HTML(page_source)
 a_lst = tree.xpath('//div[@class="tab_content"]/a/@href')
+full_lst = []
 for a_ in a_lst:
     # print(a_)
     #一级页面中二级页面链接获取成功之后，接着来获取二级页面的数据链接，需要逆向
     sec_a = urljoin(url,a_)
+
     print(sec_a)
+    # writer.writerow([sec_a,])
+    full_lst.append(sec_a)
 # print(page_source)
+print(full_lst,len(full_lst))
+# writer.writerow(full_lst)
+# fp.close()
